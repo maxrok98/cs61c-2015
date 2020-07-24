d8ddf7560ecf45120d036e7044883ceec609514c
max <max.rok.98@gmail.com>
2020-07-24 11:39:25 +0300

    Complete lab4

---
+++ labs/lab4/first1pos.s
@@ -0,0 +1,123 @@
+.data
+shouldben1:	.asciiz "Should be -1, and firstposshift and firstposmask returned: "
+shouldbe0:	.asciiz "Should be 0 , and firstposshift and firstposmask returned: "
+shouldbe16:	.asciiz "Should be 16, and firstposshift and firstposmask returned: "
+shouldbe31:	.asciiz "Should be 31, and firstposshift and firstposmask returned: "
+
+.text
+main:
+	la	$a0, shouldbe31
+	jal	print_str
+	lui	$a0, 0x8000	# should be 31
+	jal	first1posshift
+	move	$a0, $v0
+	jal	print_int
+	jal	print_space
+
+	lui	$a0, 0x8000
+	jal	first1posmask
+	move	$a0, $v0
+	jal	print_int
+	jal	print_newline
+
+	la	$a0, shouldbe16
+	jal	print_str
+	lui	$a0, 0x0001	# should be 16
+	jal	first1posshift
+	move	$a0, $v0
+	jal	print_int
+	jal	print_space
+
+	lui	$a0, 0x0001
+	jal	first1posmask
+	move	$a0, $v0
+	jal	print_int
+	jal	print_newline
+
+	la	$a0, shouldbe0
+	jal	print_str
+	li	$a0, 1		# should be 0
+	jal	first1posshift
+	move	$a0, $v0
+	jal	print_int
+	jal	print_space
+
+	li	$a0, 1
+	jal	first1posmask
+	move	$a0, $v0
+	jal	print_int
+	jal	print_newline
+
+	la	$a0, shouldben1
+	jal	print_str
+	move	$a0, $0		# should be -1
+	jal	first1posshift
+	move	$a0, $v0
+	jal	print_int
+	jal	print_space
+
+	move	$a0, $0
+	jal	first1posmask
+	move	$a0, $v0
+	jal	print_int
+	jal	print_newline
+
+	li	$v0, 10
+	syscall
+
+first1posshift:
+	### YOUR CODE HERE ###
+	add $v0, $0, 31
+	ori $t1, $0, 0x80000000
+	bne $a0, $0, good_to_shift
+	addi $v0, $0, -1
+	j ret
+
+good_to_shift:
+	and $t0, $a0, $t1
+	bne $t0, $0, ret
+	add $v0, $v0, -1
+	sll $a0, $a0, 1
+	bne $v0, $0, good_to_shift
+ret:
+	jr $ra
+
+first1posmask:
+	### YOUR CODE HERE ###
+	add $v0, $0, 31
+	ori $t1, $0, 0x80000000
+	
+	bne $a0, $0, loop_shifting
+	addi $v0, $0, -1
+	j ret
+
+loop_shifting:
+	and $t0, $a0, $t1
+	bne $t0, $0, ret
+	add $v0, $v0, -1
+	srl $t1, $t1, 1
+	j loop_shifting
+	
+
+print_int:
+	move	$a0, $v0
+	li	$v0, 1
+	syscall
+	jr	$ra
+
+print_str:
+	li	$v0, 4
+	syscall
+	jr	$ra
+
+print_space:
+	li	$a0, ' '
+	li	$v0, 11
+	syscall
+	jr	$ra
+
+print_newline:
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+	jr	$ra
